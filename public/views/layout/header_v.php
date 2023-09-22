8d4359ecbfd2d8bcdccfaa1a18adab0dce921176
Ramon Najera <ramonnajera@hotmail.com>
2023-09-22 12:55:20 -0600

    navbar responsive

--- public/views/layout/header_v.php
+++ public/views/layout/header_v.php
@@ -16,13 +16,11 @@
 </head>
 <body class="font-[Poppins] min-h-screen flex flex-col justify-start">
     
-<nav class="bg-[#45484c] border-gray-200 py-2.5">
+<!-- <nav class="bg-[#45484c] border-gray-200 py-2.5">
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
         <a href="<?=base_url?>" class="flex items-center">
-            <!-- <img src="https://www.svgrepo.com/show/499962/music.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo"> -->
             
             <img src="https://teachers-xi.vercel.app/images/logo-white.png" class="h-6 mr-3 sm:h-9" >
-            <span class="ml-8 self-center text-xl whitespace-nowrap dark:text-white">Insignias</span>
         </a>
         <div class="flex items-center lg:order-2">
             <div class="hidden mt-2 mr-4 sm:inline-block">
@@ -40,11 +38,11 @@
                 <form action="<?=htmlspecialchars(base_url . "User/login")?>" method="post">
                     <div class="mb-3 px-5">
                         <label for="user" class="input-label">Correo</label>
-                        <input type="email" id="user" name="user" class="input-text" placeholder="rnajera@uach.mx" required>
+                        <input type="email" id="userlog" name="user" class="input-text" placeholder="rnajera@uach.mx" required>
                     </div>
                     <div class="mb-5 px-5">
                         <label for="pass" class="input-label">Contraseña</label>
-                        <input type="password" id="pass" name="pass" class="input-text" required>
+                        <input type="password" id="passlog" name="pass" class="input-text" required>
                     </div>
                     <div class=" pb-10 flex justify-center">
                         <button data-close-modal class="btn  border border-gray-500 text-black hover:text-white hover:border-transparent hover:bg-[#D5175E] mx-5">Cerrar</button>
@@ -97,15 +95,87 @@
                 <li>
                     <a href="#"
                         class="block py-2 pl-3 pr-4 text-white rounded hover:text-[#D5175E]  lg:p-0"
-                        aria-current="page">Cursos</a>
+                        aria-current="page">menu 1</a>
                 </li>
                 <li>
                     <a href="#"
-                        class="block py-2 pl-3 pr-4 text-white rounded hover:text-[#D5175E]  lg:p-0">Otros</a>
+                        class="block py-2 pl-3 pr-4 text-white rounded hover:text-[#D5175E]  lg:p-0">menu 2</a>
                 </li>
                 <?php endif;?>
             </ul>
         </div>
+    </div>
+</nav> -->
+
+<nav class="flex items-center justify-between px-12 h-16 lg:gap-5 bg-[#45484c] border-gray-200">
+<a href="<?=base_url?>" class="flex items-center">    
+    <img src="https://teachers-xi.vercel.app/images/logo-white.png" class="h-6 mr-3 sm:h-9" >
+</a>
+    <div class="dropdown-menu absolute top-[64px] left-0 bg-[#45484c] w-full lg:flex lg:justify-between flex-col lg:flex-row gap-6 items-center py-2 text-lg font-bold hidden z-[2] lg:static">
+        <ul class="text-white flex flex-col lg:flex-row items-center gap-6 hover:transition-all">
+        <?php if(isset($_SESSION['identidad'])):?>
+                <?php if(isset($_SESSION['admin'])):?>
+            <li>
+                <a href="<?=htmlspecialchars(base_url)?>"
+                    class="block py-2 pl-3 pr-4 text-white rounded hover:text-[#D5175E]  lg:p-0"hover:text-[#D5175E] 
+                    aria-current="page">Mis cursos</a>
+            </li>
+            <li>
+                <a href="<?=htmlspecialchars(base_url . "Page/carreras")?>"
+                    class="block py-2 pl-3 pr-4 text-white rounded hover:text-[#D5175E]  lg:p-0">Rutas de aprendizaje</a>
+            </li>
+                <?php elseif(isset($_SESSION['user'])):?>
+                    <li>
+                        <a href="<?=htmlspecialchars(base_url)?>"
+                        class="block py-2 pl-3 pr-4 text-white  rounded hover:text-[#D5175E]  lg:p-0"
+                        aria-current="page">Mis insignias</a>
+                    </li>
+                    <li>
+                        <a href="<?=htmlspecialchars(base_url . "Curso/alls")?>"
+                            class="block py-2 pl-3 pr-4 text-white rounded hover:text-[#D5175E]  lg:p-0">Todos los cursos</a>
+                    </li>
+                <?php endif;?>
+        <?php elseif(!isset($_SESSION['identidad'])):?>
+            <li>
+                <a href="#"
+                    class="block py-2 pl-3 pr-4 text-white rounded hover:text-[#D5175E]  lg:p-0"
+                    aria-current="page">menu 1</a>
+            </li>
+            <li>
+                <a href="#"
+                    class="block py-2 pl-3 pr-4 text-white rounded hover:text-[#D5175E]  lg:p-0">menu 2</a>
+            </li>
+        <?php endif;?>
+        </ul>
+        <div class="flex flex-col lg:flex-row items-center gap-6 hover:transition-all mt-6 lg:mt-0">
+        <?php if(isset($_SESSION['identidad'])):?>
+            <a href="<?=htmlspecialchars(base_url . "User/logout")?>" class="btn btn-warning mx-3"><i class="fa-solid fa-arrow-right-from-bracket"></i> Salir</a>
+            <?php elseif(!isset($_SESSION['identidad'])):?>
+            <button data-open-modal class="btn btn-primary mx-3">Ingresar</button>
+            <a href="<?=htmlspecialchars(base_url . "Page/registro")?>"
+                class="block py-2 pl-3 pr-4 text-white border hover:bg-pink-400 hover:border-transparent ">Registro</a>
+            <dialog data-modal class="rounded-md col">
+                <p class="text-2xl mb-5 pt-10 text-center">Ingresar</p>
+                <form action="<?=htmlspecialchars(base_url . "User/login")?>" method="post">
+                    <div class="mb-3 px-5">
+                        <label for="user" class="input-label">Correo</label>
+                        <input type="email" id="userlog" name="user" class="input-text" placeholder="rnajera@uach.mx" required>
+                    </div>
+                    <div class="mb-5 px-5">
+                        <label for="pass" class="input-label">Contraseña</label>
+                        <input type="password" id="passlog" name="pass" class="input-text" required>
+                    </div>
+                    <div class=" pb-10 flex justify-center">
+                        <button data-close-modal class="btn  border border-gray-500 text-black hover:text-white hover:border-transparent hover:bg-[#D5175E] mx-5">Cerrar</button>
+                        <button type="submit" class="btn btn-primary mr-5">Entrar</button>
+                    </div>
+                </form>
+            </dialog>
+        <?php endif;?>
+        </div>
+    </div>
+    <div class="toggle-button lg:hidden">
+        <i class="fa-solid fa-bars fa-lg text-white"></i>
     </div>
 </nav>
 <?php require_once dirname(__FILE__).'../../includes/appmessages_v.php';?>
