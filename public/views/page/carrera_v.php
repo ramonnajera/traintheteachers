bd267c0832cf88dce8fd906dfa3e155c296b7f82
Bart Lopez <dalopez@uach.mx>
2023-10-23 12:13:37 -0600

    New desktop detail ruta page

--- public/views/page/carrera_v.php
+++ public/views/page/carrera_v.php
@@ -3,40 +3,40 @@
 
 <!-- Route detail -->
 
-<div class="bg-white">
+<!-- <div class="bg-white">
   <section aria-labelledby="features-heading" class="relative">
-    <div class="aspect-h-2 aspect-w-3 overflow-hidden sm:aspect-w-5 px-10 pt-7 md:py-0 md:pt-32 md:px-20 lg:aspect-none lg:absolute lg:h-full lg:w-1/1 lg:pr-4 xl:pr-16">
-      <img src="<?=base_url?>assets/img/images/<?=$carrera[0]["carrera_img"]?>" alt="" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+    <div class="aspect-h-2 aspect-w-3 overflow-hidden sm:aspect-w-5 px-10 pt-7 md:py-0 md:pt-12 md:px-auto lg:aspect-none lg:absolute lg:h-1/1 lg:w-1/3 lg:pr-4 xl:pr-0 xl:pl-24">
+      <img src="<?=base_url?>assets/img/images/<?=$carrera[0]["carrera_img"]?>" alt="" class="h-full w-full mx- lg:h-full lg:w-full">
     </div>
 
-    <div class="mx-auto max-w-2xl px-4 pb-2 pt-16 sm:px-6 sm:pb-32 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:pt-32">
-      <div class="lg:col-start-2">
+    <div class="mx-auto max-w-2xl px-4 pb-2 pt-16 sm:px-6 sm:pb-32 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:pt-3 lg:pb-60">
+      <div class="lg:col-start-2 pt-0 md:pt-12">
         <h2 id="features-heading" class="font-medium text-gray-500">Ruta de aprendizaje</h2>
         <p class="mt-4 text-4xl font-bold tracking-tight text-gray-900"><?=$carrera[0]["carrera_nombre"]?></p>
         <p class="mt-4 text-gray-500"><?=$carrera[0]["carrera_descripcion"]?></p>
-
-        <dl class="mt-10 grid grid-cols-1 gap-x-8 gap-y-10 text-sm sm:grid-cols-2">
-          <div>
-            <dt class="font-medium text-gray-900">Ruta</dt>
-            <dd class="mt-2 text-gray-500">Las rutas, se componen de talleres individuales, cada taller está pensado para otorgar al docente, el conocimiento necesario para dominar de manera básica dicha ruta.</dd>
-          </div>
-          <div>
-            <dt class="font-medium text-gray-900">Talleres</dt>
-            <dd class="mt-2 text-gray-500">Cada taller, tiene su duración y juntos consiguen reunir las 20 hrs necesarias para cumplir con los estatutos del CUDD.</dd>
-          </div>
-          <div>
-            <dt class="font-medium text-gray-900">Insignias</dt>
-            <dd class="mt-2 text-gray-500">Cada taller terminado, otorga una insignia, al reunir todas las insignias de una ruta de aprendizaje, te haces acreedor a la insignia global de la ruta. Esta insignia te acredita con las 20 hrs que pide el CUDD.</dd>
-          </div>
-          <div>
-            <dt class="font-medium text-gray-900">Práctica</dt>
-            <dd class="mt-2 text-gray-500">Todos los talleres, están pensados para que el docente, termine conociendo y pudiendo utilizar las herramientas enseñadas en dichos talleres.</dd>
-          </div>
-        </dl>
       </div>
     </div>
   </section>
+</div> -->
+
+<div class="bg-white pb-6">
+  <div aria-hidden="true" class="relative">
+    <img src="https://plus.unsplash.com/premium_photo-1664045646367-569c52c50ade?auto=format&fit=crop&q=80&w=1407&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="h-96 w-full object-cover object-center">
+    <div class="absolute inset-0 bg-gradient-to-t from-white"></div>
+  </div>
+
+  <div class="relative mx-auto -mt-60 max-w-7xl px-4 pb-16 sm:px-6 sm:pb-24 lg:px-8">
+    <div class="mx-auto max-w-2xl text-center lg:max-w-4xl">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"><?=$carrera[0]["carrera_nombre"]?></h2>
+      <p class="mt-4 text-gray-800"><?=$carrera[0]["carrera_descripcion"]?></p>
+    </div>
+
+    
+  </div>
 </div>
+
 
 <!-- Workshop list -->
 <?php require_once dirname(__FILE__).'../../includes/routes/Talleres_v.php';?>
+
+
